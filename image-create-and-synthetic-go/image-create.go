c90c9e3e9ec00966f181c2da20ea87c227139139
HidemaruOwO <hidemal@outlook.jp>
2022-12-10 20:29:14 +0900

    :sparkles: Goで画像を扱うものの追加 (image-create-and-synthetic-go/fonts/Koruri-Bold.ttf image-create-and-synthetic-go/go.mod image-create-and-synthetic-go/go.sum image-create-and-synthetic-go/image-create.go image-create-and-synthetic-go/image-synthetic.go image-create-and-synthetic-go/images/char.png image-create-and-synthetic-go/images/white_back.png)

---
+++ image-create-and-synthetic-go/image-create.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"image/png"
+	"io/ioutil"
+	"os"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+func main() {
+	// load font
+	ftBinary, err := ioutil.ReadFile("./fonts/Koruri-Bold.ttf")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	ft, err := truetype.Parse(ftBinary)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	opt := truetype.Options{
+		Size:              90,
+		DPI:               0,
+		Hinting:           0,
+		GlyphCacheEntries: 0,
+		SubPixelsX:        0,
+		SubPixelsY:        0,
+	}
+
+	imageWidth := 300
+	imageHeight := 300
+	textTopMargin := 90
+	text := "うんこ"
+
+	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
+
+	face := truetype.NewFace(ft, &opt)
+
+	dr := &font.Drawer{
+		Dst:  img,
+		Src:  image.Black,
+		Face: face,
+		Dot:  fixed.Point26_6{},
+	}
+
+	dr.Dot.X = (fixed.I(imageWidth) - dr.MeasureString(text)) / 2
+	dr.Dot.Y = fixed.I(textTopMargin)
+
+	dr.DrawString(text)
+
+	buf := &bytes.Buffer{}
+	err = png.Encode(buf, img)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	file, err := os.Create(`./images/char.png`)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	file.Write(buf.Bytes())
+}
---
+++ image-create-and-synthetic-go/image-synthetic.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"os"
+
+	"image/draw"
+	"image/png"
+)
+
+func main() {
+
+	originImagePath := "./images/white_back.png"
+	logoImagePath := "./images/char.png"
+	saveImagePath := "./synthetic.png"
+
+	originFile, err := os.Open(originImagePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	logoFile, err := os.Open(logoImagePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	originImg, _, err := image.Decode(originFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	logoImg, _, err := image.Decode(logoFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// 画像の左上のpixelの位置 w.h
+	startPointLogo := image.Point{0, 50}
+
+	logoRectangle := image.Rectangle{startPointLogo, startPointLogo.Add(logoImg.Bounds().Size())}
+	originRectangle := image.Rectangle{image.Point{0, 0}, originImg.Bounds().Size()}
+
+	rgba := image.NewRGBA(originRectangle)
+	draw.Draw(rgba, originRectangle, originImg, image.Point{0, 0}, draw.Src)
+	draw.Draw(rgba, logoRectangle, logoImg, image.Point{0, 0}, draw.Over)
+
+	out, err := os.Create(saveImagePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	png.Encode(out, rgba)
+}
