b4c702ba3e5741c745cda87ca643cd6cf692fa98
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2023-11-18 03:37:02 +0900

    Update rename_number.sh

--- bash/rename_number.sh
+++ bash/rename_number.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <directory>"
-    exit 1
-fi
-
-directory="$1"
-
-if [ ! -d "$directory" ]; then
-    echo "Error: Directory '$directory' not found."
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <directory1> [<directory2> ...]"
     exit 1
 fi
 
 IFS=$'\n'
 
-image_files=($(find "$directory" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.gif" -o -iname "*.bmp" -o -iname "*.webp" \) | sort))
+for directory in "$@"; do
+    if [ ! -d "$directory" ]; then
+        echo "Error: Directory '$directory' not found."
+        exit 1
+    fi
 
-if [ ${#image_files[@]} -eq 0 ]; then
-    echo "Error: No image files found in the specified directory."
-    exit 1
-fi
+    files=($(find "$directory" -type f | sort))
 
-counter=1
+    if [ ${#files[@]} -eq 0 ]; then
+        echo "Error: No files found in the specified directory '$directory'."
+        exit 1
+    fi
 
-for file in "${image_files[@]}"; do
-    extension="${file##*.}"
-    new_name=$(printf "%05d.%s" $counter "$extension")
-    mv "$file" "$directory/$new_name"
-    ((counter++))
+    counter=1
+
+    for file in "${files[@]}"; do
+        extension="${file##*.}"
+        new_name=$(printf "%05d.%s" $counter "$extension")
+        mv "$file" "$directory/$new_name"
+        ((counter++))
+    done
+
+    echo "Renaming complete for directory '$directory'."
 done
-
-echo "Renaming complete."
-
