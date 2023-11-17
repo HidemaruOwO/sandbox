3228ebcd07a25ee83d9ae44b7fb4f52f5ef2aad1
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2023-11-18 03:24:58 +0900

    Update convert_webp.sh

--- bash/imagemagick/convert_webp.sh
+++ bash/imagemagick/convert_webp.sh
@@ -5,23 +5,25 @@ if ! command -v magick &> /dev/null; then
   exit 1
 fi
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <directory>"
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <directory1> [directory2 ...]"
   exit 1
 fi
 
-directory=$1
-if [ ! -d "$directory" ]; then
-  echo "Specified directory does not exist: $directory"
-  exit 1
-fi
+for directory in "$@"; do
+  if [ ! -d "$directory" ]; then
+    echo "Specified directory does not exist: $directory"
+    exit 1
+  fi
 
-# Search for image files in the directory and convert them
-find "$directory" -type f \( -iname "*.avif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0 | while IFS= read -r -d $'\0' file; do
-  echo "Converten ${file}..."
-  new_file="${file%.*}.webp"
-  magick "$file" "$new_file"
-  rm "$file"
+  find "$directory" -type f \( -iname "*.avif" -o -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0 | while IFS= read -r -d $'\0' file; do
+    echo "Converting ${file}..."
+    new_file="${file%.*}.webp"
+    magick "$file" "$new_file"
+    rm "$file"
+  done
+
+  echo "Conversion for directory $directory completed."
 done
 
-echo "Conversion completed."
+echo "All conversions completed."
